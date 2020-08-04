db8761fa70b963dd9bf0a809c988b544a12c8620
TristanSmithlib <tchambers@smith.edu>
2020-08-04 09:42:43 -0400

    Readd search, and refactor list_display fields into master admin class

--- src/uploads/admin.py
+++ src/uploads/admin.py
@@ -7,19 +7,19 @@ from django.utils.safestring import mark_safe
 class UploadChildAdmin(PolymorphicChildModelAdmin):
     """ Base admin class for all child models """
     base_model = Upload  # Optional, explicitly set here.
+    search_fields = ['title', 'identifier', 'full_record_url']
+    list_display = ( 'title', 'identifier', 'created', 'modified', 'size')
 
 @admin.register(Video)
 class VideoAdmin(UploadChildAdmin):
     base_model = Video  # Explicitly set here!
 #    show_in_index = True  # makes child model admin visible in main admin site
-    list_display = ( 'title', 'identifier', 'created', 'modified', 'size')
     readonly_fields = ('size', 'created', 'modified', 'url')
 
 @admin.register(Audio)
 class AudioAdmin(UploadChildAdmin):
     base_model = Audio  # Explicitly set here!
 #    show_in_index = True  # makes child model admin visible in main admin site
-    list_display = ( 'title', 'identifier', 'created', 'modified', 'size')
     readonly_fields = ('size', 'created', 'modified', 'url')
 
 class AudioAlubmInline(admin.TabularInline):
@@ -29,7 +29,6 @@ class AudioAlubmInline(admin.TabularInline):
 class AudioAlbumAdmin(UploadChildAdmin):
     base_model = AudioAlbum  # Explicitly set here!
 #    show_in_index = True  # makes child model admin visible in main admin site
-    list_display = ( 'title', 'identifier', 'created', 'modified', 'size')
     readonly_fields = ('size', 'created', 'modified', 'url', 'album_directory')
     inlines = [AudioAlubmInline,]
 
@@ -37,7 +36,7 @@ class AudioAlbumAdmin(UploadChildAdmin):
 class TextAdmin(UploadChildAdmin):
     base_model = Text  # Explicitly set here!
 #    show_in_index = True  # makes child model admin visible in main admin site
-    list_display = ( 'title', 'identifier', 'created', 'modified', 'size')
+    list_display = ( 'title', 'text_type', 'identifier', 'created', 'modified', 'size')
     readonly_fields = ('size', 'created', 'modified', 'url', 'text_type')
     def get_readonly_fields(self, request, obj=None):
         """If obj is None that means the object is being created. In this case
@@ -58,6 +57,7 @@ class UploadParentAdmin(PolymorphicParentModelAdmin):
     child_models = (Video, Audio, AudioAlbum, Text)
     list_filter = (PolymorphicChildModelFilter,)  # This is optional.
     list_display = ( 'title', 'type', 'identifier', 'created', 'modified', 'size', 'full_record')
+    search_fields = ['title', 'identifier', 'full_record_url']
     def type(self, obj):
         return obj.polymorphic_ctype
 
