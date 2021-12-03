88664d630e3189d5c6baa710cf267e5a298b4665
Micah Walter <micahjwalter@gmail.com>
2021-12-03 12:50:34 -0500

    Revisions to management command to create uploads from csv file: don't save a duplicate copy of the video (which uses a lot of disk space), but instead add it to the log. Verify that there isn't a duplicate title, which would cause the file not to be created, and give the user a chance to supply a different title or else to skip the file, which would leave it in the import folder and add an ERROR line to the log.

--- src/uploads/management/commands/create_uploads_from_csv.py
+++ src/uploads/management/commands/create_uploads_from_csv.py
@@ -2,14 +2,16 @@ import csv
 import os.path
 import re
 import shutil
+from datetime import datetime
 from hitchcock import settings
 
 from django.core.files import File
 from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
 
 from uploads.models import Video
 
-DONE = 'done'
+LOGFILE = 'log.txt'
 
 def get_location_of_file(directory, filename, extension='mp4'):
     '''Returns the actual verified path of the video, or None if
@@ -44,6 +46,26 @@ def get_panopto_session_id(url):
     else:
         return re.match(r'.*\?id=(.*)', url).group(1)
 
+def add_video_to_database(title, notes, upload, panopto_session_id):
+    try:
+        new_video = Video(
+            title=title,
+            notes=notes,
+            upload=upload,
+            panopto_session_id=panopto_session_id,
+            lock_panopto_session_id=True,
+        )
+        new_video.save()
+        return True
+    except IntegrityError:
+        print(f"Cannot add a video with duplicate title {title}.")
+        new_title = input("Please enter new title, or ENTER to skip: ")
+        print(new_title)
+        if not new_title:
+            return False
+        else:
+            return add_video_to_database(new_title, notes, upload, panopto_session_id)
+
 
 class Command(BaseCommand):
     help = 'Add the videos in the spreadsheet given to the database'
@@ -67,33 +89,50 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         spreadsheet_file = options['spreadsheet_file'][0]
         import_directory = options['import_directory'][0]
-        # First make sure the 'done' folder is present
-        if not os.path.isdir(os.path.join(import_directory, DONE)):
-            os.mkdir(os.path.join(import_directory, DONE))
+        # First make sure the log file is present
+        # https://stackoverflow.com/a/43081892/2569052
+        log_path = os.path.join(import_directory, LOGFILE)
+        try:
+            logfile = open(log_path, 'x')
+            logfile.write('Import log\n')
+            logfile.close()
+        except FileExistsError:
+            pass
         # Now open the spreadsheet and start processing the items in it
-        with open(spreadsheet_file) as f:
+        with open(spreadsheet_file) as f, open(log_path, 'a') as logfile:
+            # Log the date and time of this run
+            logfile.write('\n' + datetime.now().strftime('%Y-%m-%d %H:%M:%S%z') + '\n')
             rows = csv.reader(f)
             for row in rows:
-                # Don't process items already in Hitchcock
                 print(row)
-                if 'hitchcock' in row[4]:
-                    continue
+                title = row[2]
+                notes = row[3]
+                upload_url = row[4]
                 file_location = get_location_of_file(import_directory, row[5])
+
+                # Don't process items already in Hitchcock
+                if 'hitchcock' in upload_url:
+                    continue
                 if not file_location:
                     continue
                 # Separate the (dir, name) tuple that makes up file_location
                 with open(file_location, 'rb') as existing_file:
                     # Split off the filename proper from the path
                     file_name = os.path.split(file_location)[1]
-                    new_video = Video(
-                        title=row[2],
-                        notes=row[3],
+                    # Results of video add saved in "skipped"
+                    success = add_video_to_database(
+                        title=title,
+                        notes=notes,
                         # specifying the name is needed so that the file
                         # goes in the media folder, rather than attempting
                         # to make a copy in the import folder
                         upload=File(existing_file, name=file_name),
-                        panopto_session_id=get_panopto_session_id(row[4]),
-                        lock_panopto_session_id=True,
+                        panopto_session_id=get_panopto_session_id(upload_url),
                     )
-                    new_video.save()
-                shutil.move(file_location, os.path.join(import_directory, DONE))
+                    if success:
+                        # Log the filename added to Hitchcock and delete file
+                        # from import location
+                        logfile.write(file_location + '\n')
+                        os.remove(file_location)
+                    else:
+                        logfile.write('ERROR: ' + file_location + '\n')
