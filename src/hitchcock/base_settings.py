c22cba75928a731a931021ce7cdfc1221d07a6e0
Micah Walter <micahjwalter@gmail.com>
2022-01-05 16:26:16 -0500

    Revert "Support for themed interfaces"
    This reverts commit 8cbcc95fc5a922bfda1f2996dd04f8b3c14e06e5.

--- src/hitchcock/base_settings.py
+++ src/hitchcock/base_settings.py
@@ -25,8 +25,6 @@ DEBUG = False
 
 INSTALLED_APPS = [
     'polymorphic',
-    'admin_interface',
-    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
